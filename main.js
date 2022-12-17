b7c3b25f46b3f2ea3a927b17c6b4b593b94af14b
Ravshan <ravshanxursanov32@gmail.com>
2022-12-17 10:10:21 +0500

    Kun soatlari

---
+++ main.js
@@ -0,0 +1,81 @@
+let hours = prompt('sizga kerak bolgan soatni kiriting!').toLowerCase()
+switch (hours) {
+    case '1':
+        console.log('Soat tungi bir');
+        break;
+    case '2':
+        console.log('Soat tungi ikki');
+        break;
+    case '3':
+        console.log('Soat tungi uch');
+        break;
+    case '4':
+        console.log(" Soat azondagi to'r ");
+        break;
+    case '5':
+        console.log(" Soat azondagi besh ");
+        break;
+    case '6':
+        console.log(" Soat kundizgi olti ");
+        break;
+    case '7':
+        console.log(" Soat kundizgi yetti ");
+        break;
+        case '8':
+            console.log(" Soat kundizgi sakkiz ");
+            break;
+            case '9':
+                console.log(" Soat kundizgi to'qqiz ");
+                break;
+                case '10':
+                    console.log(" Soat kundizgi o'n ");
+                    break;
+                    case '11':
+                        console.log(" Soat obetdagi o'n bir ");
+                        break;
+                        case '12':
+                            console.log(" Soat obetdagi o'n ikki ");
+                            break;
+                            case '13':
+                                console.log(" Soat obetdagi bir ");
+                                break;
+                                case '14':
+                                    console.log(" Soat obetdagi ikki ");
+                                    break;
+                                    case '15':
+                                        console.log(" Soat obetdagi uch ");
+                                        break;
+                                        case '16':
+                                            console.log(" Soat kechki to'r ");
+                                            break;
+                                            case '17`':
+                                                console.log('Soat kechki besh');
+                                                break; 
+                                                case '18':
+                                                console.log(" Soat kechki olti ");
+                                                break; 
+                                                case '19':
+                                                console.log(" Soat kechki yetti ");
+                                                break; 
+                                                case '20':
+                                                console.log(" Soat kechki sakkiz ");
+                                                break;
+                                                 case '21':
+                                                console.log(" Soat kechki to'qqiz ");
+                                                break;
+                                                 case '22':
+                                                console.log(" Soat tungi o'n  ");
+                                                break;
+                                                 case '23':
+                                                console.log(" Soat tungi o'n bir ");
+                                                break;
+                                                 case '24':
+                                                    console.log(" Soat tungi o'n ikki ");
+                                                break;
+                                              
+
+
+         default:
+        console.log('iltimos son kiriting');
+        break;
+}
